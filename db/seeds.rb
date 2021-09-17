487791aaa8e2683dd6c1de5e45b07228bff7cdc8
Victoria Shanukova <vshanukova@gmail.com>
2021-09-17 14:54:14 +0100

    added model, cntr, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat NYC',
+  address: '10 Clifton Gardens NYC W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Lincoln Dr. NYC SW6 6JP',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 123,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd NYC N1C 4QP',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 195,
+  number_of_guests: 3
+)
